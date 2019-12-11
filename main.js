e555cbe0d2644192ca28e6f523ab583625d7c55c
drazero <ardkokkel@outlook.com>
2019-12-11 21:13:07 +0100

    opvragen dmv knop

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 window.onload = () => {
   const box = document.getElementById('js--box');
   const text = document.getElementById('js--placeholder');
+  const input = document.getElementById('js--input');
+  const button = document.getElementById('js--button');
 
   const getKenteken = (kenteken) => {
     let merk = fetch("https://opendata.rdw.nl/resource/m9d7-ebf2.json?kenteken=" + kenteken + "&$$app_token=iGI52UvtT9HJ2ojfwD03tkjgX")
@@ -13,7 +15,13 @@ window.onload = () => {
       });
   }
 
-  box.onmouseenter = (event) => {
+  button.onclick = (event) => {
+    // console.log(input.value);
+    let kenteken = input.value;
+    getKenteken(kenteken);
+  }
+
+  box.onclick = (event) => {
     let kenteken = "XX999X";
     getKenteken(kenteken);
   }
