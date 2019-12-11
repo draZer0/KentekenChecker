760809cdefcea1d5ad4272fb575202743a11d4cc
drazero <ardkokkel@outlook.com>
2019-12-11 22:45:20 +0100

    last update js

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
 window.onload = () => {
-  const box = document.getElementById('js--box');
-  const text = document.getElementById('js--placeholder');
   const input = document.getElementById('js--input');
   const button = document.getElementById('js--button');
 
+  const kentekenContainer = document.getElementById('js--kenteken-container');
+  const textKenteken = document.getElementById('js--kenteken');
+  const textVoertuigsoort = document.getElementById('js--voertuigsoort');
+  const textMerk = document.getElementById('js--merk');
+  const textHandelsbenaming = document.getElementById('js--handelsbenaming');
+
   const getKenteken = (kenteken) => {
-    let merk = fetch("https://opendata.rdw.nl/resource/m9d7-ebf2.json?kenteken=" + kenteken + "&$$app_token=iGI52UvtT9HJ2ojfwD03tkjgX")
+    let request = fetch("https://opendata.rdw.nl/resource/m9d7-ebf2.json?kenteken=" + kenteken + "&$$app_token=iGI52UvtT9HJ2ojfwD03tkjgX")
       .then( (data) => {
           return data.json();
       })
       .then( (response) => {
-        // console.log(response[0].merk);
-        text.innerHTML = response[0].merk;
+        textKenteken.innerHTML = response[0].kenteken;
+        textVoertuigsoort.innerHTML = response[0].voertuigsoort;
+        textMerk.innerHTML = response[0].merk;
+        textHandelsbenaming.innerHTML = response[0].handelsbenaming;
       });
   }
 
   button.onclick = (event) => {
-    // console.log(input.value);
-    let kenteken = input.value;
+    let kenteken = input.value.toUpperCase();
     getKenteken(kenteken);
+    kentekenContainer.style.display = 'block';
   }
 
-  box.onclick = (event) => {
-    let kenteken = "XX999X";
-    getKenteken(kenteken);
+  input.onkeyup = (event) => {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      button.click();
+    }
   }
 }
